ff6edbba8ef70b861a59f8002da08dda34b89327
ipinto <ipinto@redhat.com>
2019-03-19 15:43:21 +0200

    Add VM resource

--- utilities/types.py
+++ utilities/types.py
@@ -4,6 +4,7 @@
 # API
 API_VERSION_ALPHA_3 = 'kubevirt.io/v1alpha3'
 API_VERSION_V1 = 'v1'
+CNV_API_VERSION = API_VERSION_ALPHA_3
 
 # Resources
 VMI = 'VirtualMachineInstance'
@@ -11,3 +12,12 @@ VM = 'VirtualMachine'
 POD = "Pod"
 NODE = 'Node'
 NAMESPACE = 'Namespace'
+
+# VMI / Pod status
+RUNNING = 'Running'
+PENDING = 'Pending'
+SUCCEEDED = 'Succeeded'
+FAILED = 'Failed'
+UNKNOWN = 'Unknown'
+COMPLETED = 'Completed'
+CRASH_LOOP_BACKOFF = 'CrashLoopBackOff'
