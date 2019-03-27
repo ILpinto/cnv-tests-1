392d576957bd7559b04fe22ded81569d3dd7bece
Meni Yakove <myakove@redhat.com>
2019-03-27 13:28:29 +0200

    Remove comment code .

--- tests/network/connectivity/conftest.py
+++ tests/network/connectivity/conftest.py
@@ -5,15 +5,6 @@ Pytest conftest file for CNV network tests
 """
 
 
-# def pytest_namespace():
-#     return {
-#         'active_node_nics': {},
-#         'nodes_network_info': {},
-#         'real_nics_env': False,
-#         'bond_support_env': False
-#     }
-
-
 def pytest_configure():
     import pytest
     pytest.active_node_nics = {}
